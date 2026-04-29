02aca5208614054f71d11bbafaab7e8e8e8f6abc
harshika <harshikakolekar84@gmail.com>
2026-04-29 21:56:46 +0530

    Created Signup Route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,3 +2,38 @@ const express=require('express');
 const router=express.Router();
 const User=require('../models/userModel');
 
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+//Signup Route
+router.post("/signup",async(req,res)=>{
+    try{
+        const {name, email, phone, password, address, role} = req.body;
+
+        const existingUser=await User.findOne({email});
+        
+        if(existingUser){
+            return res.status(400).json({message:'User already exists'});
+        }
+        
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        const user=await User.create({
+            name,
+            email,
+            phone,
+            password:hashedPassword,
+            address,
+            role
+        });
+
+        res.status(201).json({message:'Signup SUccesfull'});
+
+    } catch(error){
+        res.status(500).json({
+            message:'Signup failed'
+        })
+    };  
+});
+
+//Login Route

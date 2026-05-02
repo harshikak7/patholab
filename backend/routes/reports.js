d141201f804198f65353de923e717a1cfca9db64
harshikak7 <harshikakolekar184@gmail.com>
2026-05-02 19:12:02 +0530

    Set up cloudinary

--- backend/routes/reports.js
+++ backend/routes/reports.js
@@ -4,9 +4,13 @@ const router=express.Router();
 const verifyToken=require('../middleware/auth')
 const verifyRole=require('../middleware/role')
 
+const cloudinary=require('../config/cloudinary')
+const upload=require('../middleware/upload')
+
+const Booking=require('../models/bookingModel')
 const Report=require('../models/reportModel') 
 
-router.post('/upload',verifyToken,verifyRole('admin'),async(req,res)=>{
+router.post('/upload',verifyToken,verifyRole('admin'),upload.single('report'),async(req,res)=>{
     try{
 
         const booking=await Booking.findById(req.body.bookingId);
@@ -14,13 +18,25 @@ router.post('/upload',verifyToken,verifyRole('admin'),async(req,res)=>{
         if(!booking){
             res.status(401).json({message:'No Bookings Found'})
         }
+        
+        const result=await cloudinary.uploader.upload_stream({
+            resource_type:'auto'
+        }, async(error,uploadResult)=>{
+            if(error){
+                return res.status(500).json({message:'Upload Failed'});
+            }
 
-        const report=await Report.create({
+            const report=await Report.create({
             bookingId:booking._id,
             userId:booking.userId,
-            reportUrl:req.body.reportUrl
+            reportUrl:uploadResult.secure_url
         });
         res.status(201).json({message:'Report uploaded successfully',report});
+        stream.end(req.file.buffer);
+        })
+
+        
+
     } catch(error){
         console.log(error);
         res.status(500).json({message:'Report upload failed'})

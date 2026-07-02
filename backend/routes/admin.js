dad2c82e1dd9428236fdd91ba6a71e35e1fa1a2f
harshikak7 <harshikakolekar184@gmail.com>
2026-07-02 14:52:17 +0530

    Admin Panel Dashboard updated

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,25 +5,97 @@ const verifyToken = require("../middleware/auth");
 const verifyRole = require("../middleware/role");
 
 const Booking = require("../models/bookingModel");
+const User = require("../models/userModel");
 
-router.get(
-  "/bookings",
-  verifyToken,
-  verifyRole("admin"),
+router.get("/dashboard", verifyToken, verifyRole("admin"), async (req, res) => {
+  try {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-  async (req, res) => {
-    try {
-      const bookings = await Booking.find()
-        .populate("userId", "name email phone")
-        .populate("tests", "testName price");
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
 
-      res.json(bookings);
-    } catch (error) {
-      res.status(500).json({
-        message: "Failed",
-      });
-    }
-  },
-);
+    // Cards
+    const todayBookings = await Booking.countDocuments({
+      appointmentDate: {
+        $gte: today,
+        $lt: tomorrow,
+      },
+    });
+
+    const pendingReports = await Booking.countDocuments({
+      reportUpload: false,
+    });
+
+    const completedTests = await Booking.countDocuments({
+      status: "Completed",
+    });
+
+    const totalPatients = await User.countDocuments({
+      role: "user",
+    });
+
+    // Recent Bookings
+    const recentBookings = await Booking.find()
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .populate("userId", "name email")
+      .populate("tests", "testName");
+
+    // Status Chart
+
+    const pending = await Booking.countDocuments({
+      status: "Pending",
+    });
+
+    const assigned = await Booking.countDocuments({
+      status: "Assigned",
+    });
+
+    const collected = await Booking.countDocuments({
+      status: "Collected",
+    });
+
+    const completed = await Booking.countDocuments({
+      status: "Completed",
+    });
+
+    res.json({
+      cards: {
+        todayBookings,
+        pendingReports,
+        completedTests,
+        totalPatients,
+      },
+
+      statusChart: [
+        {
+          name: "Pending",
+          value: pending,
+        },
+        {
+          name: "Assigned",
+          value: assigned,
+        },
+        {
+          name: "Collected",
+          value: collected,
+        },
+        {
+          name: "Completed",
+          value: completed,
+        },
+      ],
+
+      recentBookings,
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: "Dashboard Failed",
+    });
+  }
+});
 
 module.exports = router;
